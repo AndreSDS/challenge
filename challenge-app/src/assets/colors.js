3e4f2e0a094a241a46c3912363a5db8eff25d68e
AndreSDS <andre_smiths@outlook.com>
2021-06-02 13:35:06 -0300

    criando arquivo de cores e reset css global

---
+++ challenge-app/src/assets/colors.js
@@ -0,0 +1,11 @@
+const COLORS = {
+  blue100: '#96B9FD',
+  blue200: '#7BA4FC',
+  blue300: '#5081FB',
+  darkblue: '#051D3B',
+  white: '#fff',
+  gray6: '#666',
+  lightgreen: '#9AFF6B'
+}
+
+export default COLORS;
